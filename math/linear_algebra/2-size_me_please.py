ea5363213558afb90d92624e03222191a06d63ed
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-12-03 11:49:50 -0400

    need documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """module to calculate the shape of a matrix"""
+
+
 def matrix_shape(matrix):
     """calculates the shape of a matrix, and must return a list of integers"""
     shape = []
