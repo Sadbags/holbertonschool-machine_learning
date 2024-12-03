b07fa1c97f8aa22d34ed6fd6c82a90ffd646367a
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-12-03 12:29:37 -0400

    fixed pycodestyle

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""module to concatenate two matrices along a specific axis"""
+"""Module to add two 2D matrices of the same dimensions."""
 
 
 def add_matrices2D(mat1, mat2):
-    """adds two matrices"""
+    """Adds two 2D matrices element by element."""
     if len(mat1) != len(mat2):
         return None
     if len(mat1[0]) != len(mat2[0]):
         return None
     return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
-			for i in range(len(mat1))]
-    if axis == 0:
-        return [add_matrices2D(row1, row2) for row1, row2 in zip(mat1, mat2)]
-    else:
-        return [[add_matrices2D([mat1[i][j] for i in range(len(mat1))], mat2[j][i]) for j in range(len(mat2[0]))]
-				for i in range(len(mat2))]
-
+            for i in range(len(mat1))]
