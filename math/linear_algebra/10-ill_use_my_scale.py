5bbeb19b016a1c4f67f75567a0f77ffdc7c013a0
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-12-04 12:15:02 -0400

    checking if task is correct

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+""" module that calculates the shape of a numpy.ndarray """
+
+
+def np_shape(matrix):
+    """ calculates the shape of a numpy.ndarray"""
+    return matrix.shape
