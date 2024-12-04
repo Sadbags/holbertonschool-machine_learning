66a08126946561160203d6adfdb869773e374c7c
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-12-04 12:34:01 -0400

    running checks for task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,10 +4,6 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """ concatenates two matrices along a specific axis """
-    if axis == 0:
-        return np.concatenate((mat1, mat2), axis=axis)
-    elif axis == 1:
-        return np.concatenate((mat1, mat2), axis=axis)
-    else:
-        return None
+    """ concatenates two matrices along a
+    specific axis with no conditional statements """
+    return np.concatenate((mat1, mat2), axis=axis)
