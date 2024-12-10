d4cae4ba457a475247b7611c9f4a40312ad91184
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-12-10 12:29:50 -0400

    the graph apeared, now runnign checks

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """ plot y as a line graph: """
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
     """ plot y as a line graph: """
+
+
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
