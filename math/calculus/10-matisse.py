63467bba64e04d8703d79ceaa21d68235d00abbb
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-12-12 00:45:00 -0400

    adding task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# function that calculates the derivative of polynominal
+
+def poly_derivative(poly):
+    """ calculates the derivative of polynominal"""
+    if not poly or not isinstance(poly, list):
+        return None
+
+    if len(poly) == 1:
+        return [0]
+
+    derivative = []
+
+    for i in range(1, len(poly)):
+        derivative.append(i * poly[i])
+
+    if not derivative:
+        return [0]
+
+    return derivative
