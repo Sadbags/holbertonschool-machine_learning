d8ead71bb5dbaf750836cd5a78dbc498b2164527
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-01-02 14:17:39 -0400

    task 4

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+""" module defines Neuron """
+import numpy as np
+
+
+class Neuron:
+    """ defines a single neuron  """
+    def __init__(self, nx):
+        """ This function initializes the Neuron instance """
+        if isinstance(nx, int) is False:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        # Attributes
+        self.__W = np.random.randn(nx).reshape(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Getter for the weights vector """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter for the bias """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter for the activated output """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ This function calculates the forward propagation of the neuron """
+        w_sum = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-w_sum))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ calculates the cost """
+        m = Y.shape[1]
+        logprobs = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = -1 / m * np.sum(logprobs)
+        return cost
+
+    def evaluate(self, X, Y):
+		""" Evaluates the neuron's predictions """
+		self.forward_prop(X)
+        cost = self.cost(Y, self.__A)
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+        return prediction, cost
