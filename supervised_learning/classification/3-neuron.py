8c5ceb0530e2a17f0caf6394b0b6c966560f4e2f
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-01-02 14:14:04 -0400

    task 3

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python3
-""" module for neuron class"""
-
+""" module defines Neuron """
 import numpy as np
 
 
 class Neuron:
-    """ class to create a neuron """
+    """ defines a single neuron  """
     def __init__(self, nx):
-        """ initializes the neuron """
-        if not isinstance(nx, int):
-            raise TypeError('nx must be an integer')
+        """ This function initializes the Neuron instance """
+        if isinstance(nx, int) is False:
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+            raise ValueError("nx must be a positive integer")
+        # Attributes
+        self.__W = np.random.randn(nx).reshape(1, nx)
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
-        """ weights vector attributes """
+        """ Getter for the weights vector """
         return self.__W
 
     @property
     def b(self):
-        """ for the bias """
+        """ Getter for the bias """
         return self.__b
 
+    @property
     def A(self):
-        """ activated output """
+        """ Getter for the activated output """
         return self.__A
 
     def forward_prop(self, X):
-        """ calculates the forward propagation """
+        """ This function calculates the forward propagation of the neuron """
         w_sum = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-w_sum))
         return self.__A
 
     def cost(self, Y, A):
-        """ calculates de cost of the model """
+        """ calculates the cost """
         m = Y.shape[1]
         logprobs = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
         cost = -1 / m * np.sum(logprobs)
